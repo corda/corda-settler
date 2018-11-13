33e3ee25b237e3e62dbc0e5c23f20dcc782ddf20
RogerWillis <roger.willis@r3cev.com>
2018-11-13 15:23:01 +0000

    Added error handling wrt. timeouts.

--- cordapp-contracts-states/src/main/kotlin/net/corda/finance/obligation/flows/AbstractSendToSettlementOracle.kt
+++ cordapp-contracts-states/src/main/kotlin/net/corda/finance/obligation/flows/AbstractSendToSettlementOracle.kt
@@ -2,7 +2,14 @@ package net.corda.finance.obligation.flows
 
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.InitiatingFlow
+import net.corda.core.serialization.CordaSerializable
 import net.corda.core.transactions.SignedTransaction
 
 @InitiatingFlow
 abstract class AbstractSendToSettlementOracle : FlowLogic<SignedTransaction>()
+
+@CordaSerializable
+sealed class OracleResult {
+    data class Success(val stx: SignedTransaction) : OracleResult()
+    data class Failure(val message: String) : OracleResult()
+}

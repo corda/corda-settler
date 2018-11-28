732634066490b0aea7766d5ade79a10403b7c329
RogerWillis <roger.willis@r3cev.com>
2018-11-28 09:11:21 +0000

    Remove front end from master branch.

--- app/src/main/kotlin/com/r3/corda/finance/obligation/app/controllers/GetSettlementMethods.kt
+++
@@ -1,6 +0,0 @@
-package com.r3.corda.finance.obligation.app.controllers
-
-class GetSettlementMethods {
-    // Scans for subclasses of make off ledger payment.
-    // These can be returned to the UI.
-}

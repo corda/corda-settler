d579a17d733cd6d243e713a60e6c338455754f30
Thor Gilman <thorgilman@MacBook-Dev-13.local>
2019-08-20 10:31:37 -0400

    Final changes for SWIFT gpi update

--- oracle/src/main/kotlin/com/r3/corda/finance/obligation/oracle/flows/VerifySettlement.kt
+++ oracle/src/main/kotlin/com/r3/corda/finance/obligation/oracle/flows/VerifySettlement.kt
@@ -31,8 +31,8 @@ class VerifySettlement(private val otherSession: FlowSession) : FlowLogic<Unit>(
     companion object {
         private const val INITIAL_TIME_TO_WAIT_FOR_SETTLEMENT = 3L
         private const val RETRY_TIME_TO_WAIT_FOR_SETTLEMENT = 5L
+        private const val SIGNATURE_STATUS_SIGNED = "Signed"
     }
-
 
     enum class VerifyResult { TIMEOUT, SUCCESS, PENDING, REJECTED }
 
@@ -63,10 +63,7 @@ class VerifySettlement(private val otherSession: FlowSession) : FlowLogic<Unit>(
             when (paymentStatus.transactionStatus.status) {
                 SWIFTPaymentStatusType.RJCT -> return VerifyResult.REJECTED
                 SWIFTPaymentStatusType.ACCC -> {
-//                    val originalAmt = swiftPayment.amount
-//                    val confirmedAmt = amount(paymentStatus.transactionStatus.confirmedAmount.toBigDecimal(), originalAmt.token)
-//                    if (originalAmt != confirmedAmt) throw FlowException("SWIFT recorded the wrong amount.")
-                    if (paymentStatus.transactionStatus.signatureStatus != "Signed") throw FlowException("SWIFT payment not signed.")
+                    if (paymentStatus.transactionStatus.signatureStatus != SIGNATURE_STATUS_SIGNED) throw FlowException("SWIFT payment not signed.")
                     return VerifyResult.SUCCESS
                 }
                 // TODO: we need to come up with some more clever way of waiting for the status to be updated. Maybe exponential back-off
